ee1d66885c56506c659bf4f4e030be411f8d4258
Heni BEN AMARA <63763172+henibenamara@users.noreply.github.com>
2026-09-20 21:52:16 +0100

    Create exceptions.dart

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,12 @@
+/// Thrown by data sources when storage access fails.
+///
+/// Exceptions never leave the data layer: repositories catch them and return
+/// a `Failure` instead.
+class StorageException implements Exception {
+  const StorageException(this.message);
+
+  final String message;
+
+  @override
+  String toString() => 'StorageException: ' + message;
+}
